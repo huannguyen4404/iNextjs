e668398b8fc245b09a4f2a981d16e7a2e1fb5e36
huan <huannguyen4404@gmail.com>
2023-05-02 22:11:35 +0700

    [C-02] automatic static optimization

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 
 export interface AboutProps {}
 
 export default function About(props: AboutProps) {
+  const router = useRouter()
+  console.log('About query: ', router.query)
+
   return <div>About Us</div>
+}
+
+// with this one, this page will be SSR
+export async function getServerSideProps() {
+  return {
+    props: {}, // will be passed to the page component as props
+  }
 }
