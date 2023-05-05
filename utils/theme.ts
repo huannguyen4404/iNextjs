298081f11840c2b247c8c8712efd7e0a5a9ef7c2
huan <huannguyen4404@gmail.com>
2023-05-05 21:56:51 +0700

    [C-04] Setup MUI

---
+++ utils/theme.ts
@@ -0,0 +1,25 @@
+import { Roboto } from 'next/font/google'
+import { createTheme } from '@mui/material'
+import { red } from '@mui/material/colors'
+
+export const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+})
+
+// Create a theme instance.
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FF6464',
+    },
+    secondary: {
+      main: '#00A8CC',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+})
