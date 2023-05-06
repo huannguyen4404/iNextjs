1ba6cbd0822d67e89cb5334f5ece853a6f2dc61e
huan <huannguyen4404@gmail.com>
2023-05-06 21:55:56 +0700

    [C-04] update footer

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,9 +1,32 @@
-import { Box } from '@mui/material'
+import { Facebook, Instagram, LinkedIn, Twitter } from '@mui/icons-material'
+import { Box, Icon, Stack, Typography } from '@mui/material'
 
 export function Footer() {
+  const socialUrls = [
+    { icon: Facebook, url: 'https://google.com' },
+    { icon: Instagram, url: 'https://instagram.com' },
+    { icon: Twitter, url: 'https://google.com' },
+    { icon: LinkedIn, url: 'https://google.com' },
+  ]
+
   return (
     <Box component="footer" py={2} textAlign="center">
-      Footer
+      <Stack direction="row" justifyContent="center">
+        {socialUrls.map((item, idx) => (
+          <Box
+            key={idx}
+            component="a"
+            p={2}
+            href={item.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon component={item.icon} sx={{ fontSize: 48 }} />
+          </Box>
+        ))}
+      </Stack>
+
+      <Typography>Copyright ©{new Date().getFullYear()} All rights reserved</Typography>
     </Box>
   )
 }
