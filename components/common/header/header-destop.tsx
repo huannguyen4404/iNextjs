080c6d113725d59476f0ee4402047a3dbc22b497
huan <huannguyen4404@gmail.com>
2023-05-07 23:51:11 +0700

    [C-06] menu with auth state

--- components/common/header/header-destop.tsx
+++ components/common/header/header-destop.tsx
@@ -3,17 +3,22 @@ import { useRouter } from 'next/router'
 import { ROUTE_LIST } from './routes'
 import clsx from 'clsx'
 import Link from 'next/link'
+import { useAuth } from '@/hooks'
 
 export interface HeaderDesktopProps {}
 
 export function HeaderDesktop(props: HeaderDesktopProps) {
   const router = useRouter()
+  const { profile, logout } = useAuth()
+  const isLoggedIn = Boolean(profile?.username)
+
+  const routeList = ROUTE_LIST.filter((route) => !route.requireLogin || isLoggedIn)
 
   return (
     <Box display={{ xs: 'none', md: 'block' }} py={2}>
       <Container>
         <Stack direction="row" justifyContent="flex-end">
-          {ROUTE_LIST.map((route) => (
+          {routeList.map((route) => (
             <Link key={route.path} href={route.path} passHref legacyBehavior>
               <LinkMui
                 sx={{ ml: 2, fontWeight: 'medium' }}
@@ -23,6 +28,18 @@ export function HeaderDesktop(props: HeaderDesktopProps) {
               </LinkMui>
             </Link>
           ))}
+
+          {!isLoggedIn && (
+            <Link key="login" href="/login" passHref legacyBehavior>
+              <LinkMui sx={{ ml: 2, fontWeight: 'medium' }}>Login</LinkMui>
+            </Link>
+          )}
+
+          {isLoggedIn && (
+            <LinkMui sx={{ ml: 2, fontWeight: 'medium', cursor: 'pointer' }} onClick={logout}>
+              Logout
+            </LinkMui>
+          )}
         </Stack>
       </Container>
     </Box>
