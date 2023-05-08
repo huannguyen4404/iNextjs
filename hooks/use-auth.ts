0f2a95c5a183ff1cb96369eedc9ab635bc1cdd20
huan <huannguyen4404@gmail.com>
2023-05-08 20:24:30 +0700

    [C-06] save profile in localStorage

--- hooks/use-auth.ts
+++ hooks/use-auth.ts
@@ -1,17 +1,34 @@
+import { StorageKeys } from '@/constants'
 import { LoginPayload, UserProfile } from '@/models'
 import { authApi } from '@/services'
 import useSWR, { SWRConfiguration } from 'swr'
 
+function getUserInfo(): UserProfile | null {
+  try {
+    return JSON.parse(localStorage.getItem(StorageKeys.USER_INFO) || '')
+  } catch (error) {
+    console.log('Failed to parse user info from localstorage', error)
+    return null
+  }
+}
+
 export function useAuth(options?: SWRConfiguration) {
-  const {
-    data: profile,
-    error,
-    mutate,
-  } = useSWR<UserProfile | null>(`/profile`, {
+  const swrOptions: SWRConfiguration = {
     dedupingInterval: 60 * 60 * 1000,
     revalidateOnFocus: false,
     ...options,
-  })
+    fallbackData: getUserInfo(),
+    onSuccess(data) {
+      localStorage.setItem(StorageKeys.USER_INFO, JSON.stringify(data))
+    },
+    onError(err) {
+      // sentry, ddtrace...
+      console.log(err)
+      logout()
+    },
+  }
+
+  const { data: profile, error, mutate } = useSWR<UserProfile | null>(`/profile`, swrOptions)
 
   const firstLoading = profile === undefined && error === undefined
 
@@ -22,8 +39,8 @@ export function useAuth(options?: SWRConfiguration) {
 
   async function logout() {
     await authApi.logout()
-
     mutate(null, false)
+    localStorage.removeItem(StorageKeys.USER_INFO)
   }
 
   return { profile, error, login, logout, firstLoading }
